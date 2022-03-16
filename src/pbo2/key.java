a956b2b6103c8c98eec0c52252ae0ddfaa307a1b
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-03-16 14:44:44 +0800

    Add form Konsumen

---
+++ src/pbo2/key.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2;
+
+/**
+ *
+ * @author XXIVKVNGLRT
+ */
+public interface key {
+    
+    
+String username = "root";
+String password = "";
+String driver = "com.mysql.jdbc.Driver";
+String url = "jdbc:mysql://localhost:3306/pos_toko";
+}
+
+    
+
